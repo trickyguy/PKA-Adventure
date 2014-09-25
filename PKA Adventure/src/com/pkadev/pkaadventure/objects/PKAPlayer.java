bb43221b0015d43724714cc2498c9e78db3c6923
TwoPointDuck <theodorschuetz@gmail.com>
2014-09-25 16:47:00 +0200

    Pasting my version to fix error

--- PKA Adventure/src/com/pkadev/pkaadventure/objects/PKAPlayer.java
+++ PKA Adventure/src/com/pkadev/pkaadventure/objects/PKAPlayer.java
@@ -20,7 +20,7 @@ import com.pkadev.pkaadventure.utils.MessageUtil;
 
 public class PKAPlayer extends PKALivingEntity {
 
-	public PKAPlayer(Player player, ClassType classType, int level, int experience, int maxHealth,
+	public PKAPlayer(Player player, ClassType classType, int level, int experience, int maxHealth, 
 			int health, double damage, int weaponSlot, int availableUpgradePoints, int miningExp, int miningLevel, int gold,
 			List<String> discoveredLocations) {
 		super(player.getName(), level, damage, maxHealth, health);
@@ -30,13 +30,13 @@ public class PKAPlayer extends PKALivingEntity {
 		setWeaponSlot(weaponSlot);
 		setAvailableUpgradePoints(availableUpgradePoints);
 		setDiscoveredLocations(discoveredLocations);
-
+		
 		setMiningExp(miningExp); // miningExp
 		setMiningLevel(miningLevel); // miningLevel
 		setGoldAmount(gold);
 	}
-
-	public PKAPlayer(Player player, ClassType classType, int level, int experience, int maxHealth,
+	
+	public PKAPlayer(Player player, ClassType classType, int level, int experience, int maxHealth, 
 			int health, double damage, int weaponSlot, int availableUpgradePoints, int miningExp, int miningLevel, int gold,
 			List<String> discoveredLocations, List<String> activeQuests, List<String> finishedQuests) {
 		super(player.getName(), level, damage, maxHealth, health);
@@ -48,12 +48,12 @@ public class PKAPlayer extends PKALivingEntity {
 		setDiscoveredLocations(discoveredLocations);
 		setActiveQuests(QuestProcessor.getQuestMap(activeQuests, player.getName()));
 		setFinishedQuests(finishedQuests);
-
+		
 		setMiningExp(miningExp); // miningExp
 		setMiningLevel(miningLevel); // miningLevel
 		setGoldAmount(gold);
 	}
-
+	
 	private Player player;
 	private ClassType classType;
 	private int experience;
@@ -71,20 +71,20 @@ public class PKAPlayer extends PKALivingEntity {
 	private List<String> discoveredLocations = new ArrayList<String>();
 	private HashMap<String, PKAQuest> activeQuests = new HashMap<String, PKAQuest>();
 	private List<String> finishedQuests = new ArrayList<String>();
-
+	
 	private int goldAmount;
-
-
+	
+	
 	//TODO EXPERIMENTAL
 	private int cachedGoldAmount = 0;
 	private boolean hasGoldTask = false;
-
-
-
+	
+	
+	
 	// EXP, Level
 	private int miningExp;
 	private int miningLevel;
-
+	
 	public Player getPlayer() {
 		return player;
 	}
@@ -131,7 +131,7 @@ public class PKAPlayer extends PKALivingEntity {
 	 * @param currentlySelectedAbility
 	 * @return abilityName
 	 */
-	 public String setCurrentlySelectedAbility(Integer currentlySelectedAbility) {
+	public String setCurrentlySelectedAbility(Integer currentlySelectedAbility) {
 		Ability ability = getAbilities().get(currentlySelectedAbility);
 		if (ability == null) {
 			if (getAbilities().get(this.currentlySelectedAbility) != null) {
@@ -142,130 +142,130 @@ public class PKAPlayer extends PKALivingEntity {
 		}
 		this.currentlySelectedAbility = currentlySelectedAbility;
 		return getAbilities().get(currentlySelectedAbility).getName();
-	 }
-	 public int getWeaponSlot() {
-		 return weaponSlot;
-	 }
-	 public void setWeaponSlot(int weaponSlot) {
-		 this.weaponSlot = weaponSlot;
-	 }
-	 public int getAvailableUpgradePoints() {
-		 return availableUpgradePoints;
-	 }
-	 public void setAvailableUpgradePoints(int availableUpgradePoints) {
-		 this.availableUpgradePoints = availableUpgradePoints;
-	 }
-	 public void addAvailableUpgradePoint() {
-		 availableUpgradePoints += 1;
-	 }
-	 public void removeAvailableUpgradePoint() {
-		 availableUpgradePoints -= 1;
-	 }
-	 public int getExperience() {
-		 return experience;
-	 }
-	 public void setExperience(int experience) {
-		 this.experience = experience;
-	 }
-	 public void addExperience(int experience) {
-		 experience += experience;
-	 }
-	 public int getExperienceRequired() {
-		 return experienceRequired;
-	 }
-	 public void setExperienceRequired(int experienceRequired) {
-		 this.experienceRequired = experienceRequired;
-	 }
-	 public int getNoDamageTicksTaken() {
-		 return noDamageTicksTaken;
-	 }
-	 public void setNoDamageTicksTaken(int noDamageTicksTaken) {
-		 this.noDamageTicksTaken = noDamageTicksTaken;
-	 }
-	 public int getNoDamageTicksGiven() {
-		 return noDamageTicksGiven;
-	 }
-	 public void setNoDamageTicksGiven(int noDamageTicksGiven) {
-		 this.noDamageTicksGiven = noDamageTicksGiven;
-	 }
-	 public List<String> getDiscoveredLocations() {
-		 return discoveredLocations;
-	 }
-	 public void discoverLocation(String locationName) {
-		 discoveredLocations.add(locationName);
-	 }
-	 public void setDiscoveredLocations(List<String> discoveredLocations) {
-		 this.discoveredLocations = discoveredLocations;
-	 }
-	 public int getMiningExp() {
-		 return miningExp;
-	 }
-	 public void setMiningExp(int experience) {
-		 this.miningExp = experience;
-	 }
-	 public int getMiningLevel() {
-		 return miningLevel;
-	 }
-	 public void setMiningLevel(int level) {
-		 this.miningLevel = level;
-	 }
-	 public int getGoldAmount() {
-		 return goldAmount;
-	 }
-	 public void setGoldAmount(int gold) {
-		 this.goldAmount = gold;
-	 }
-	 public void addGoldAmount(int gold) {
-		 if (!hasGoldTask) {
-			 cachedGoldAmount = gold;
-			 hasGoldTask = true;
-			 Bukkit.getScheduler().scheduleSyncDelayedTask(Main.instance, new Runnable() {
+	}
+	public int getWeaponSlot() {
+		return weaponSlot;
+	}
+	public void setWeaponSlot(int weaponSlot) {
+		this.weaponSlot = weaponSlot;
+	}
+	public int getAvailableUpgradePoints() {
+		return availableUpgradePoints;
+	}
+	public void setAvailableUpgradePoints(int availableUpgradePoints) {
+		this.availableUpgradePoints = availableUpgradePoints;
+	}
+	public void addAvailableUpgradePoint() {
+		availableUpgradePoints += 1;
+	}
+	public void removeAvailableUpgradePoint() {
+		availableUpgradePoints -= 1;
+	}
+	public int getExperience() {
+		return experience;
+	}
+	public void setExperience(int experience) {
+		this.experience = experience;
+	}
+	public void addExperience(int experience) {
+		experience += experience;
+	}
+	public int getExperienceRequired() {
+		return experienceRequired;
+	}
+	public void setExperienceRequired(int experienceRequired) {
+		this.experienceRequired = experienceRequired;
+	}
+	public int getNoDamageTicksTaken() {
+		return noDamageTicksTaken;
+	}
+	public void setNoDamageTicksTaken(int noDamageTicksTaken) {
+		this.noDamageTicksTaken = noDamageTicksTaken;
+	}
+	public int getNoDamageTicksGiven() {
+		return noDamageTicksGiven;
+	}
+	public void setNoDamageTicksGiven(int noDamageTicksGiven) {
+		this.noDamageTicksGiven = noDamageTicksGiven;
+	}
+	public List<String> getDiscoveredLocations() {
+		return discoveredLocations;
+	}
+	public void discoverLocation(String locationName) {
+		discoveredLocations.add(locationName);
+	}
+	public void setDiscoveredLocations(List<String> discoveredLocations) {
+		this.discoveredLocations = discoveredLocations;
+	}
+	public int getMiningExp() {
+		return miningExp;
+	}
+	public void setMiningExp(int experience) {
+		this.miningExp = experience;
+	}
+	public int getMiningLevel() {
+		return miningLevel;
+	}
+	public void setMiningLevel(int level) {
+		this.miningLevel = level;
+	}
+	public int getGoldAmount() {
+		return goldAmount;
+	}
+	public void setGoldAmount(int gold) {
+		this.goldAmount = gold;
+	}
+	public void addGoldAmount(int gold) {
+		if (!hasGoldTask) {
+			cachedGoldAmount = gold;
+			hasGoldTask = true;
+			Bukkit.getScheduler().scheduleSyncDelayedTask(Main.instance, new Runnable() {
 
-				 @Override
-				 public void run() {
-					 MessageUtil.sendMessage(player, "ง6+" + cachedGoldAmount + " (" + goldAmount + ")", MessageType.SIMPLE);
-					 hasGoldTask = false;
-				 }
+				@Override
+				public void run() {
+					MessageUtil.sendMessage(player, "ยง6+" + cachedGoldAmount + " (" + goldAmount + ")", MessageType.SIMPLE);
+					hasGoldTask = false;
+				}
+				
+			}, 20l);
+		} else {
+			cachedGoldAmount += gold;
+		}
+		goldAmount += gold;
+	}
+	public boolean removeGoldAmount(int gold) {
+		if (goldAmount - gold < 0)
+			return false;
+		goldAmount -= gold;
+		return true;
+	}
 
-			 }, 20l);
-		 } else {
-			 cachedGoldAmount += gold;
-		 }
-		 goldAmount += gold;
-	 }
-	 public boolean removeGoldAmount(int gold) {
-		 if (goldAmount - gold < 0)
-			 return false;
-		 goldAmount -= gold;
-		 return true;
-	 }
+	public HashMap<String, PKAQuest> getActiveQuests() {
+		return activeQuests;
+	}
 
-	 public HashMap<String, PKAQuest> getActiveQuests() {
-		 return activeQuests;
-	 }
+	public void addActiveQuest(String questReference, PKAQuest pkaQuest) {
+		activeQuests.put(questReference, pkaQuest);
+	}
+	
+	public void removeActiveQuest(String questReference) {
+		activeQuests.remove(questReference);
+	}
+	
+	private void setActiveQuests(HashMap<String, PKAQuest> activeQuests) {
+		this.activeQuests = activeQuests;
+	}
 
-	 public void addActiveQuest(String questReference, PKAQuest pkaQuest) {
-		 activeQuests.put(questReference, pkaQuest);
-	 }
-
-	 public void removeActiveQuest(String questReference) {
-		 activeQuests.remove(questReference);
-	 }
-
-	 private void setActiveQuests(HashMap<String, PKAQuest> activeQuests) {
-		 this.activeQuests = activeQuests;
-	 }
-
-	 public List<String> getFinishedQuests() {
-		 return finishedQuests;
-	 }
-
-	 public void addFinishedQuest(String questReference) {
-		 finishedQuests.add(questReference);
-	 }
-
-	 private void setFinishedQuests(List<String> finishedQuests) {
-		 this.finishedQuests = finishedQuests;
-	 }
-
+	public List<String> getFinishedQuests() {
+		return finishedQuests;
+	}
+	
+	public void addFinishedQuest(String questReference) {
+		finishedQuests.add(questReference);
+	}
+	
+	private void setFinishedQuests(List<String> finishedQuests) {
+		this.finishedQuests = finishedQuests;
+	}
+	
 }
